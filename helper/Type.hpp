c5ab05a7efd1e4129a7e6f306ebd5ec09be29301
Ghozali <riz.ghz@gmail.com>
2023-03-10 11:04:43 +0700

    Update helper/Type.hpp

---
+++ helper/Type.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace Type {
+
+/* 8-bit Signed Integer Type */
+typedef signed char i8;
+/* 8-bit Unsigned Integer Type */
+typedef unsigned char u8;
+
+/* 16-bit Signed Integer Type */
+typedef signed short i16;
+/* 16-bit Unsigned Integer Type */
+typedef unsigned short u16;
+
+/* 32-bit Signed Integer Type */
+typedef signed int i32;
+/* 32-bit Unsigned Integer Type */
+typedef unsigned int u32;
+
+/* 64-bit Signed Integer Type */
+typedef signed long long i64;
+/* 64-bit Unsigned Integer Type */
+typedef unsigned long long u64;
+
+/* 128-bit Signed Integer Type */
+/* 128-bit Unsigned Integer Type */
+
+/* 32-bit Floating-point Type */
+typedef float f32;
+
+/* 64-bit Floating-point Type */
+typedef double f64;
+
+/* 128-bit Floating-point Type */
+// typedef long double f128;
+
+/* Pointer-Sized Signed Integer Type */
+typedef i64 isize;
+
+/* Pointer-Sized Unsigned Integer Type */
+typedef u64 usize;
+
+/* 8-bit Unsigned Integer Type */
+typedef u8 byte;
+
+/* C-Style String */
+typedef char* str;
+
+} /* namespace Type */
